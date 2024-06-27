f00aaa1685d8b6098830015f94247a1ce14a83ee
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-06-27 07:57:19 +0300

    Updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; 
+//const shortGreeting = `Welcome, ${currentUser[0]}!`; 
+
+console.log(shortGreeting);
